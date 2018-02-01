bdc8399e69edf3be4107d61fd080d2cae3894ea1
Haris Kljajic <devhariis@gmail.com>
2018-02-01 23:19:39 +0100

    Changed this to getActivity

--- app/src/main/java/com/virtualathlete/myfitness/feed/FeedFragment.kt
+++ app/src/main/java/com/virtualathlete/myfitness/feed/FeedFragment.kt
@@ -47,7 +47,7 @@ class FeedFragment @Inject constructor() : DaggerFragment(), WorkoutViewAdapter.
         // Get the ViewModel.
         viewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java)
 
-        viewModel.getWorkouts().observe(this, Observer { workouts ->
+        viewModel.getWorkouts().observe(activity, Observer { workouts ->
             workouts?.let { workoutAdapter.swapWorkouts(it) }
         })
     }
@@ -65,7 +65,7 @@ class FeedFragment @Inject constructor() : DaggerFragment(), WorkoutViewAdapter.
         workout_floating_action_button.setOnClickListener(this)
         sleep_floating_action_button.setOnClickListener(this)
         swipe_refresh_layout.setOnRefreshListener({
-            viewModel.getWorkouts().observe(this, Observer { workouts ->
+            viewModel.getWorkouts().observe(activity, Observer { workouts ->
                 workouts?.let { workoutAdapter.swapWorkouts(it) }
                 swipe_refresh_layout.isRefreshing = false
             })
